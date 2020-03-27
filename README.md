7c6b7050c0c0c44dc5dcac5a6c2e0382c0381959
kirsty <kirstypurkhardt@gmail.com>
2020-03-27 14:42:51 +0000

    kirstys made chanegs to index.js

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 ## This is the READme.md
 # I am for reading. 
+
+kirsty added changes to index,js
--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 let a = 2;
 let b = 3;
 
+let h = 1;
+let i = 5;
+
 console.log(a+b);
+console.log(h + i);
